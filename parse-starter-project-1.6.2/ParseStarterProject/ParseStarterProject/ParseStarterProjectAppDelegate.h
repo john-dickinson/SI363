40fb15df3afdf4c108f499f568343dbbcd584410
john-dickinson <johndi@umich.edu>
2015-01-30 11:30:32 -0500

    starter project

---
+++ parse-starter-project-1.6.2/ParseStarterProject/ParseStarterProject/ParseStarterProjectAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ParseStarterProjectAppDelegate.h
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@class ParseStarterProjectViewController;
+
+@interface ParseStarterProjectAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@property (nonatomic, strong) IBOutlet ParseStarterProjectViewController *viewController;
+
+@end
